2c54e4867951ac2e1bf133447262219c0d170307
Priyabrata <samal7244@gmail.com>
2021-03-24 09:57:01 +0530

    flask app start

---
+++ flask/flask_cv1/server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/template/<name>.html')
+def page(name):
+    return render_template(f'{name}.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
